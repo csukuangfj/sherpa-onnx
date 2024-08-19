6a0871f21e29299d1ca1cb9b81d0d0f1eb7335eb
Fangjun Kuang <csukuangfj@gmail.com>
2024-08-19 17:24:39 +0800

    support onnxruntime 1.19.0

--- cmake/onnxruntime-linux-x86_64.cmake
+++ cmake/onnxruntime-linux-x86_64.cmake
@@ -14,19 +14,19 @@ if(NOT BUILD_SHARED_LIBS)
   message(FATAL_ERROR "This file is for building shared libraries. BUILD_SHARED_LIBS: ${BUILD_SHARED_LIBS}")
 endif()
 
-set(onnxruntime_URL  "https://github.com/csukuangfj/onnxruntime-libs/releases/download/v1.17.1/onnxruntime-linux-x64-glibc2_17-Release-1.17.1-patched.zip")
-set(onnxruntime_URL2 "https://hub.nuaa.cf/csukuangfj/onnxruntime-libs/releases/download/v1.17.1/onnxruntime-linux-x64-glibc2_17-Release-1.17.1-patched.zip")
-set(onnxruntime_HASH "SHA256=cb90c51a195bdd453aaf1582f3ef63b466dafbb15c4b8a552ca4dce3769e1d1e")
+set(onnxruntime_URL  "https://github.com/csukuangfj/onnxruntime-libs/releases/download/v1.19.0/onnxruntime-linux-x64-glibc2_17-Release-1.19.0.zip")
+set(onnxruntime_URL2 "https://hub.nuaa.cf/csukuangfj/onnxruntime-libs/releases/download/v1.19.0/onnxruntime-linux-x64-glibc2_17-Release-1.19.0.zip")
+set(onnxruntime_HASH "SHA256=7252619ee97ede7b14d1adc2f327950c4d0e747ac985705822ebd21d94043771")
 
 # If you don't have access to the Internet,
 # please download onnxruntime to one of the following locations.
 # You can add more if you want.
 set(possible_file_locations
-  $ENV{HOME}/Downloads/onnxruntime-linux-x64-glibc2_17-Release-1.17.1-patched.zip
-  ${CMAKE_SOURCE_DIR}/onnxruntime-linux-x64-glibc2_17-Release-1.17.1-patched.zip
-  ${CMAKE_BINARY_DIR}/onnxruntime-linux-x64-glibc2_17-Release-1.17.1-patched.zip
-  /tmp/onnxruntime-linux-x64-glibc2_17-Release-1.17.1-patched.zip
-  /star-fj/fangjun/download/github/onnxruntime-linux-x64-glibc2_17-Release-1.17.1-patched.zip
+  $ENV{HOME}/Downloads/onnxruntime-linux-x64-glibc2_17-Release-1.19.0.zip
+  ${CMAKE_SOURCE_DIR}/onnxruntime-linux-x64-glibc2_17-Release-1.19.0.zip
+  ${CMAKE_BINARY_DIR}/onnxruntime-linux-x64-glibc2_17-Release-1.19.0.zip
+  /tmp/onnxruntime-linux-x64-glibc2_17-Release-1.19.0.zip
+  /star-fj/fangjun/download/github/onnxruntime-linux-x64-glibc2_17-Release-1.19.0.zip
 )
 
 foreach(f IN LISTS possible_file_locations)
